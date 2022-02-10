38c94859e0451c9d8417d86e760dc23cf724de82
Gegy <gegy1000@gmail.com>
2022-02-10 10:01:27 +0200

    increase player mixin priority (closes #52)
    this ensures the player-role copy logic runs before Drogtor's own copy logic, which otherwise causes an exception due to uninitialized roles

--- src/main/java/dev/gegy/roles/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/dev/gegy/roles/mixin/ServerPlayerEntityMixin.java
@@ -18,7 +18,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ServerPlayerEntity.class)
+@Mixin(value = ServerPlayerEntity.class, priority = 900)
 public abstract class ServerPlayerEntityMixin extends PlayerEntity implements PlayerWithRoles {
     @Unique
     private PlayerRoleSet playerRoleSet;
