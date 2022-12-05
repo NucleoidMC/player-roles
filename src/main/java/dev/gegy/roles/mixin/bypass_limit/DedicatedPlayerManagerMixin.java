2b80dfc03af6ef2ce6d0eba2a8c84303e70af2a4
williambl <william@williambl.com>
2022-12-05 19:55:25 +0000

    Fix compilation error from a 1.19.3 -> 1.19.2 change

--- src/main/java/dev/gegy/roles/mixin/bypass_limit/DedicatedPlayerManagerMixin.java
+++ src/main/java/dev/gegy/roles/mixin/bypass_limit/DedicatedPlayerManagerMixin.java
@@ -2,11 +2,10 @@ package dev.gegy.roles.mixin.bypass_limit;
 
 import com.mojang.authlib.GameProfile;
 import dev.gegy.roles.PlayerRoles;
-import net.minecraft.registry.CombinedDynamicRegistries;
-import net.minecraft.registry.ServerDynamicRegistryType;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.dedicated.DedicatedPlayerManager;
+import net.minecraft.util.registry.DynamicRegistryManager;
 import net.minecraft.world.WorldSaveHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(DedicatedPlayerManager.class)
 public class DedicatedPlayerManagerMixin extends PlayerManager {
-    public DedicatedPlayerManagerMixin(MinecraftServer server, CombinedDynamicRegistries<ServerDynamicRegistryType> registryManager, WorldSaveHandler saveHandler, int maxPlayers) {
+    public DedicatedPlayerManagerMixin(MinecraftServer server, DynamicRegistryManager.Immutable registryManager, WorldSaveHandler saveHandler, int maxPlayers) {
         super(server, registryManager, saveHandler, maxPlayers);
     }
 
