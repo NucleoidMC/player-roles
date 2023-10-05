fb6e58146b98142e37a5a87706cc041e487c4b2e
haykam821 <24855774+haykam821@users.noreply.github.com>
2023-10-05 10:56:05 -0400

    Update mod to Minecraft 1.20.2

--- src/main/java/dev/gegy/roles/mixin/PlayerManagerMixin.java
+++ src/main/java/dev/gegy/roles/mixin/PlayerManagerMixin.java
@@ -3,6 +3,7 @@ package dev.gegy.roles.mixin;
 import dev.gegy.roles.store.PlayerRoleManager;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ConnectedClientData;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(PlayerManager.class)
 public class PlayerManagerMixin {
     @Inject(method = "onPlayerConnect", at = @At("HEAD"))
-    private void onPlayerConnect(ClientConnection connection, ServerPlayerEntity player, CallbackInfo ci) {
+    private void onPlayerConnect(ClientConnection connection, ServerPlayerEntity player, ConnectedClientData clientData, CallbackInfo ci) {
         var roleManager = PlayerRoleManager.get();
         roleManager.onPlayerJoin(player);
     }
