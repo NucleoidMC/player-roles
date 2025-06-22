b558c0cf48db7a8f80a24f54d8ca798a59852860
haykam821 <24855774+haykam821@users.noreply.github.com>
2025-06-22 05:03:24 -0400

    Update mod to Minecraft 1.21.6 (#106)

--- src/main/java/dev/gegy/roles/mixin/entity_selectors/EntitySelectorReaderMixin.java
+++ src/main/java/dev/gegy/roles/mixin/entity_selectors/EntitySelectorReaderMixin.java
@@ -4,17 +4,17 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import dev.gegy.roles.PlayerRoles;
 import dev.gegy.roles.api.PlayerRolesApi;
-import net.minecraft.command.CommandSource;
 import net.minecraft.command.EntitySelectorReader;
+import net.minecraft.command.PermissionLevelSource;
 import net.minecraft.server.command.ServerCommandSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(EntitySelectorReader.class)
 public class EntitySelectorReaderMixin {
-    @WrapOperation(method = "shouldAllowAtSelectors", at = @At(value = "INVOKE", target = "Lnet/minecraft/command/CommandSource;hasPermissionLevel(I)Z"))
-    private static boolean hasPermissionLevel(CommandSource source, int level, Operation<Boolean> original) {
-        if (original.call(source, level)) {
+    @WrapOperation(method = "shouldAllowAtSelectors", at = @At(value = "INVOKE", target = "Lnet/minecraft/command/PermissionLevelSource;hasElevatedPermissions()Z"))
+    private static boolean hasPermissionLevel(PermissionLevelSource source, Operation<Boolean> original) {
+        if (original.call(source)) {
             return true;
         }
 
